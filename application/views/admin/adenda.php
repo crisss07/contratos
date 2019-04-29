206f7739e2ebd9f8dd7f1b6f14ebc800c0bad51b
cristian <rodri07crisss@gmail.com>
2019-04-29 18:33:48 -0400

    adenda

--- application/views/admin/adenda.php
+++ application/views/admin/adenda.php
@@ -1,93 +1,18 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-if ($tipo=='reg'){
-  $fTit='Crear Boleta';
-  $fSpa='';
-  $tip='';
-  $cat='';
-  $af='';
-  $em='';
-  if(isset($id) && $id!=''){
-    $fTit='Crear Ampliación';
-    foreach ($boleta as $row) {
-      $tip=$row->tipo;
-      $cat=$row->categoria;
-      $af=$row->afianzado;
-      $em=$row->empresa;
-    }    
-  }
-}
-if ($tipo=='upd'){
-  $fTit='Editar Boleta';
-  if ($vigencia!='no') {
-    foreach($boleta as $row){
-      if($row->fin!='')
-          $dif = $row->dif2;
-      else
-        $dif = $row->dif1;
-      if($row->estado!=2){
-        if($dif<=15){
-          if( $dif>10)
-            $fSpa = '<span style="font-size:14px;" class="label bg-blue">'.$dif.' dias</span>';
-          else{
-            if ($dif>5) 
-              $fSpa = '<span style="font-size:14px;" class="label bg-yellow">'.$dif.' dias</span>';
-            else{
-              if($dif>=0)
-                $fSpa = '<span style="font-size:14px;" class="label bg-red">'.$dif.' dias</span>';
-              else
-                $fSpa = '<span style="font-size:14px;" class="label bg-black">'.$dif.' dias</span>';
-            }
-          }
-        }
-        else{
-          $fSpa = '<span style="font-size:14px;" class="label bg-green">'.$dif.' dias</span>';
-        }
-      }
-      else
-        $fSpa = '<span style="font-size:14px;" class="label bg-green">Liberado</span>';
-    }
-  }
-  else{
-   $fTit='Editar Ampliación'; 
-   $fSpa=''; 
-  }
-  foreach ($boleta as $res) {
-    if($res->respaldo!=''){
-      $fSpa .= '<a class="btn btn-social btn-success" style="float:right;" href="'.base_url('assets/respaldo/'.$res->respaldo).'" target="_blank"><i class="fa fa-file-pdf-o"></i> Descargar Respaldo</a>';
-    }
-    else{
-      $fSpa .= '<a class="btn btn-social btn-danger" style="float:right;" href="javascript:;"><i class="fa fa-file-pdf-o"></i> Sin Respaldo</a>';
-    }
-  }
-}if ($tipo=='regU'){
-  $fTit='Crear Usuario';
-  $fSpa='';
-}
-if ($tipo=='updU'){
-  $fTit='Actualizar Usuario';
-  $fSpa='';
-}
-?>
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      <?php echo $fTit.' '.$fSpa;?>
-    </h1>
-  </section>
-<?php if($tipo=='reg'){?>
+
+
   <!-- Main content -->
   <section class="content">
-    <?php $att = array('id' => 'formBR','class'=>'','autocomplete'=>'off','enctype'=>'multipart/form-data'); echo form_open('boleta/add_boleta',$att); ?>
+   <?php echo form_open('Adenda/insertar', array('method'=>'POST')); ?>
       <div class="row">
         <!-- left column -->
         <div class="col-md-12">
           <!-- general form elements -->
           <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Datos generales</h3>
+              <h3 class="box-title">Crear Adenda</h3>
             </div>
               <div class="box-body">
                 <div class="row">
@@ -98,21 +23,17 @@ if ($tipo=='updU'){
                         <div class="col-sm-6">
                           <div class="form-group">
                             <label>Tipo</label>
-                            <select id="tipo" name="tipo" class="form-control" required <?php if($tip!=''){echo 'disabled';}?>>
-                              <option disabled <?php if($tip==''){echo 'selected';}?>>Selecciona Tipo</option>
-                              <option value="Boleta" <?php if($tip=='Boleta'){echo 'selected';}?>>Boleta</option>
-                              <option value="Poliza" <?php if($tip=='Poliza'){echo 'selected';}?>>Poliza</option>
-                            </select>
+                            <input type="text" value="<?php echo $contrato->tipo; ?>" name="tipo" class="form-control" readonly>
+                              
                           </div>
                         </div>
                         <div class="col-sm-6">
                           <div class="form-group">
                             <label>Tipo de Adenda</label>
-                            <select id="cat" name="cat" class="form-control" required <?php if($cat!=''){echo 'disabled';}?>>
-                              <option disabled <?php if($cat==''){echo 'selected';}?>>Selecciona Categoria</option>
-                              <option value="Cumplimiento de Contrato" <?php if($cat=='Cumplimiento de Contrato'){echo 'selected';}?>>Cumplimiento de Contrato</option>
-                              <option value="Seriedad de Propuesta" <?php if($cat=='Seriedad de Propuesta'){echo 'selected';}?>>Seriedad de Propuesta</option>
-                              <option value="Correcta Inversion de Anticipo" <?php if($cat=='Correcta Inversion de Anticipo'){echo 'selected';}?>>Correcta Inversion de Anticipo</option>
+                            <select id="categoria" name="categoria" class="form-control" required>
+                              <option disabled>Selecciona Categoria</option>
+                              <option value="Ampliacion de Plazo" >Ampliacion de Plazo</option>
+                              <option value="Ampliacion de Alcance">Ampliacion de Alcance</option>
                             </select>
                           </div>
                         </div>
@@ -120,26 +41,26 @@ if ($tipo=='updU'){
                     </div>
                     <div class="form-group">
                       <label for="afi">Beneficiario:</label>
-                      <input <?php if($af!=''){?>readonly<?php }?> type="text" class="form-control" id="afi" name="afi" placeholder="Ingresar a cuenta de" required value="<?php echo $af;?>">
+                      <input readonly type="text" class="form-control" id="beneficiario" name="beneficiario" placeholder="Ingresar a cuenta de" required value="<?php echo $contrato->beneficiario;?>">
                     </div>
                     <div class="form-group">
                       <label for="emp">Contratante:</label>
-                      <input <?php if($em!=''){?>readonly<?php }?> type="text" class="form-control" id="emp" name="emp" placeholder="Ingresar nombre" required value="<?php echo $em;?>">
+                      <input readonly type="text" class="form-control" id="empresa" name="empresa" placeholder="Ingresar nombre" required value="<?php echo $contrato->empresa;?>">
                     </div>    
                   </div>
                   <div class="col-md-6">
-                    <label for="exampleInputPassword1">Datos del Pago:</label>
+                    <label for="exampleInputPassword1">Fuente de Financiamiento:</label>
                     <div class="form-group">
-                      <input type="text" class="form-control" id="enf" name="enf" placeholder="Entidad Financiera" required>
+                      <input readonly type="text" class="form-control" value="<?php echo $contrato->ent_financiera; ?>" id="ent_financiera" name="ent_financiera" required>
                     </div>
-                    <div class="form-group">
-                      <input type="text" class="form-control" id="npl" name="npl" placeholder="Codigo" required>
+                    <div class="form-group">N&uacte;mero de Contrato
+                      <input readonly type="text" class="form-control" value="<?php echo $contrato->no_contrato ?>" id="no_contrato" name="no_contrato" required>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputPassword1">Monto:</label>
                       <div class="row">
                         <div class="col-sm-6">
-                          <input type="text" class="form-control" id="bs" name="bs" placeholder="Monto" required>
+                          <input type="text" class="form-control" id="monto" name="monto" placeholder="Monto" required>
                         </div>
                         <div class="col-sm-6">
                           <div class="form-group">
@@ -155,7 +76,7 @@ if ($tipo=='updU'){
                     </div>
                     <div class="form-group">
 					    <label>Respaldo PDF:</label>
-					    <input type="file" accept="application/pdf" size="5" class="form-control" id="res" name="res" placeholder="Agregar Respaldo">
+					    <input type="file" accept="application/pdf" size="5" class="form-control" id="respaldo" name="respaldo" placeholder="Agregar Respaldo">
 					</div>
                   </div>
                 </div>
@@ -171,14 +92,17 @@ if ($tipo=='updU'){
                 <!-- form start -->
                   <div class="box-body">
                     <div class="form-group">
-                      <textarea class="form-control" id="obj" name="obj" rows="3" placeholder="Objeto" required></textarea>
+                      <textarea class="form-control" id="objeto" name="objeto" rows="3" placeholder="Objeto" required></textarea>
                     </div>
                     <div class="form-group">
-                      <textarea class="form-control" id="obs" name="obs" rows="3" placeholder="Observaciones"></textarea>
+                      <textarea class="form-control" id="supervicion" name="supervicion" rows="3" placeholder="Observaciones"></textarea>
                     </div>
                   </div>
               </div>    
             </div>
+              <div class="form-group">
+                        <input type="text" hidden="" id="id_contrato" name="id_contrato" value="<?php echo $contrato->id_contrato; ?>">
+              </div>
             <div class="col-md-6">
               <div class="box box-primary">
                 <div class="box-header with-border">
@@ -197,6 +121,8 @@ if ($tipo=='updU'){
                       </div>
                       <!-- /.input group -->
                     </div>
+
+                  
                     <div class="box-footer">
                       <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
@@ -209,10 +135,10 @@ if ($tipo=='updU'){
       </div>
       <?php if(isset($id) && $id!=''){$vid=$id;}else{$vid='no';}?>
       <input type="text" id="id" name="id" hidden="" value="<?php echo$vid;?>" required>
-    <?php echo form_close(); ?>
+   </form>
     <!-- /.row -->
   </section>
-<?php }?>
+
 <?php if($tipo=='upd'){?>
   <section class="content">
       <div class="row">
