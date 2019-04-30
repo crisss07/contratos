cbdc77fc5a77304beac4caa12346719f1e3d550c
rodrigo <rodrigosecko@gmail.com>
2019-04-30 09:58:29 -0400

    querys 5-10-15

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Boletas</title>
+  <title>Contratos</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css');?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css');?>">
